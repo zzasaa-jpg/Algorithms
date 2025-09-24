9f26190cfd9db7e1a9396cdf9d7e77037668416c
Mahendra890() <130951660+zzasaa-jpg@users.noreply.github.com>
2025-09-24 21:12:52 +0530

    Update copy.js
    Corrected misspelling.

--- src/copy.js
+++ src/copy.js
@@ -1,6 +1,6 @@
 function copy_(code_value, copy_btn) {
 	  navigator.clipboard.writeText(code_value);
-	  copy_btn.innerText = "Copyed!";
+	  copy_btn.innerText = "Copied!";
 	  copy_btn.disabled = true;
 	  setTimeout(()=>{
 		  copy_btn.disabled = false;
